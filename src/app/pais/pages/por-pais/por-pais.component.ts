efa97802da633ddce8bd0da8bb5f165e3dc4e300
Carlos Interiano <carinthnd@gmail.com>
2021-09-10 16:24:50 -0600

    Creación de interfaz Country, componente pais-tabla

--- src/app/pais/pages/por-pais/por-pais.component.ts
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+
+import { Country } from '../../interfaces/pais.interface';
+
+import { PaisService } from '../../services/pais.service';
 
 @Component({
   selector: 'app-por-pais',
@@ -6,11 +10,30 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class PorPaisComponent implements OnInit {
+export class PorPaisComponent {
+  termino: string = '';
+  hayError: boolean = false;
+  paises: Country[] = [];
 
-  constructor() { }
+  constructor(
+    private paisService: PaisService
+  ) { }
 
-  ngOnInit(): void {
+  buscar() {
+    this.hayError = false;
+
+    if(this.termino != ''){
+      this.paisService.buscarPais(this.termino)
+        .subscribe( ( paises ) => {
+          this.paises = paises;
+          console.log(this.paises);
+        }, (err) => {
+          this.hayError = true;
+          this.paises = [];
+        })
+    }
+      
+    // this.termino = '';
   }
 
 }
